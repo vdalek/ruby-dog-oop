cfeee6cdfd8bb404fc7b09463318064229ab6b32
Dale Keliikoa <vdalek@hotmail.com>
2016-07-05 23:04:33 -0700

    Add files via upload

---
+++ dog.rb
@@ -0,0 +1,18 @@
+require_relative 'mammal'
+class Dog < Mammal
+
+	def pet
+		@health += 5
+		self
+	end
+	def walk
+		@health -= 1
+		self
+	end
+	def run
+		@health -=10
+		self
+	end
+end
+
+Dog.new.walk.walk.walk.run.run.pet.display_health
